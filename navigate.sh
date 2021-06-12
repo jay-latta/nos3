98feaa0e3e436aae9c9e73475bf94a47520e3f29
Jay Latta <jay.latta@tmctechnologies.com>
2021-06-11 20:42:13 -0400

    playing with appspec

--- navigate.sh
+++ navigate.sh
@@ -1,2 +1,10 @@
-cd nos3
-make
+cd /nos3 &&
+cd .. &&
+cd opt/nos3 &&
+sudo chown $USER 42/ &&
+cd .. &&
+cd .. &&
+cd nos3 &&
+sudo chmod a+x gsw/scripts/launch.sh &&
+sudo chmod a+x gsw/scripts/stop.sh &&
+sudo make
